7ce8eb87169b0cb7649f3db6913dc4831d30b83d
joaovpedroso <joao_victor.dp@hotmail.com>
2024-06-30 21:50:04 -0300

    chore: add pipeline tests

--- src/utils/formatters/date.ts
+++ src/utils/formatters/date.ts
@@ -1,5 +1,5 @@
 const formatDate = (date: string): string =>
-    new Date(`${date} 00:00:00`).toLocaleDateString("pr-BR");
+    new Date(`${date} 00:00:00`).toLocaleDateString("pt-BR");
 
 
 export { formatDate };
